606aa526183505e6c21703b9b98718cd6bf3b9b4
Minopon0626 <bahxn934@gmail.com>
2024-07-21 16:56:53 +0900

    import変更

--- integration_test/main.py
+++ integration_test/main.py
@@ -15,8 +15,8 @@ from file_system import create_or_find_output  # create_or_find_outputモジュ�
 from ir import Infrared_rays_send  # Infrared_rays_sendモジュールをインポート
 from capture import time_capture  # time_captureモジュールをインポート
 import shutil  # shutilモジュールをインポートして、ファイル操作を行う
-from algorithm.algorithm import process_data, get_initial_location  # algorithm.pyから必要な関数をインポート
-from algorithm.file_manager import create_directories, update_data_file  # ディレクトリ作成とデータ更新関数をインポート
+from algorithm import algorithm  # algorithm.pyから必要な関数をインポート
+from algorithm import file_manager  # ディレクトリ作成とデータ更新関数をインポート
 
 def main():
     # ディレクトリが存在しない場合は作成
@@ -36,11 +36,11 @@ def main():
         return
 
     # 初期位置情報を取得
-    location = get_initial_location()
+    location = algorithm.get_initial_location()
     # 室内温度のデフォルト値
     room_temperature = 25.0  
     status = 0  # 状態のデフォルト値 (unknown)
-    directory_paths = create_directories()  # ディレクトリ作成
+    directory_paths = file_manager.create_directories()  # ディレクトリ作成
 
     while True:  # 無限ループを開始
         # 画像を撮影
@@ -81,10 +81,10 @@ def main():
                 
                 print(f"総計 - 半袖: {short_sleeve_count}, 長袖: {long_sleeve_count}, 不明: {unknown_count}")
                 
-                cooling_threshold, heating_threshold, status = process_data(room_temperature, number_of_people, long_sleeve_count, short_sleeve_count, status, location, directory_paths)
+                cooling_threshold, heating_threshold, status = algorithm.process_data(room_temperature, number_of_people, long_sleeve_count, short_sleeve_count, status, location, directory_paths)
                 
                 if cooling_threshold is not None and heating_threshold is not None:
-                    update_data_file(room_temperature, cooling_threshold, heating_threshold, status, number_of_people, directory_paths)
+                    file_manager.update_data_file(room_temperature, cooling_threshold, heating_threshold, status, number_of_people, directory_paths)
                 
                 Infrared_rays_send.send_ir_command()
                 # 人が1人以上検出された場合、赤外線コマンドを送信する
