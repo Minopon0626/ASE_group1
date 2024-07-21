0fbdc4658738e66ed50f4755d5f62e8dc2cf1903
285112f <285112f@gs.kochi-tech.ac.jp>
2024-07-21 18:12:39 +0900

    LEDadd

--- pushbutton.py
+++ pushbutton.py
@@ -6,12 +6,17 @@ import sys
 # ポート番号の定義
 Sw_GPIO_1 = 23
 Sw_GPIO_2 = 21
+LED_GPIO_1 = 24
+LED_GPIO_2 = 25
 
 # GPIOの設定
 GPIO.setmode(GPIO.BCM)
 # GPIO23とGPIO21を入力モードに設定してプルダウン抵抗を有効にする
 GPIO.setup(Sw_GPIO_1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(Sw_GPIO_2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+# GPIO24とGPIO25を出力モードに設定
+GPIO.setup(LED_GPIO_1, GPIO.OUT)
+GPIO.setup(LED_GPIO_2, GPIO.OUT)
 
 # デバウンス処理のための時間（秒）
 debounce_time = 0.02  # 20ミリ秒
@@ -26,9 +31,11 @@ while True:
             # 再度スイッチの状態を確認
             if GPIO.input(Sw_GPIO_1) == 1:
                 print("samui")
+                GPIO.output(LED_GPIO_1, GPIO.HIGH)  # LEDを点灯
                 # スイッチが離されるまで待機
                 while GPIO.input(Sw_GPIO_1) == 1:
                     time.sleep(0.1)
+                GPIO.output(LED_GPIO_1, GPIO.LOW)  # LEDを消灯
         
         # GPIO21の入力を読み取る
         switchStatus_2 = GPIO.input(Sw_GPIO_2)
@@ -38,9 +45,11 @@ while True:
             # 再度スイッチの状態を確認
             if GPIO.input(Sw_GPIO_2) == 1:
                 print("atui")
+                GPIO.output(LED_GPIO_2, GPIO.HIGH)  # LEDを点灯
                 # スイッチが離されるまで待機
                 while GPIO.input(Sw_GPIO_2) == 1:
                     time.sleep(0.1)
+                GPIO.output(LED_GPIO_2, GPIO.LOW)  # LEDを消灯
         
         time.sleep(0.1)
 
