2e607c40f2f6a5e58b39722e563afa747006df14
285112f <285112f@gs.kochi-tech.ac.jp>
2024-07-21 19:04:55 +0900

    修正

--- pushbutton.py
+++ pushbutton.py
@@ -7,7 +7,6 @@ import sys
 Sw_GPIO_1 = 23
 Sw_GPIO_2 = 21
 LED_GPIO_1 = 24
-LED_GPIO_2 = 25
 
 # GPIOの設定
 GPIO.setmode(GPIO.BCM)
@@ -16,7 +15,6 @@ GPIO.setup(Sw_GPIO_1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(Sw_GPIO_2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 # GPIO24とGPIO25を出力モードに設定
 GPIO.setup(LED_GPIO_1, GPIO.OUT)
-GPIO.setup(LED_GPIO_2, GPIO.OUT)
 
 # デバウンス処理のための時間（秒）
 debounce_time = 0.02  # 20ミリ秒
@@ -30,14 +28,16 @@ while True:
         switchStatus_2 = GPIO.input(Sw_GPIO_2)
         
         if switchStatus_1 == 1 and switchStatus_2 == 1:
+            time.sleep(debounce_time)
+            if GPIO.input(Sw_GPIO_1) == 1 and GPIO.input(Sw_GPIO_2) == 1:
             # 両方のスイッチが押されている場合
-            print("same time")
-            GPIO.output(LED_GPIO_1, GPIO.HIGH)  # LEDを点灯
-            GPIO.output(LED_GPIO_2, GPIO.HIGH)  # LEDを点灯
-            time.sleep(blink_interval)  # 点灯時間
-            GPIO.output(LED_GPIO_1, GPIO.LOW)   # LEDを消灯
-            GPIO.output(LED_GPIO_2, GPIO.LOW)   # LEDを消灯
-            time.sleep(blink_interval)  # 消灯時間
+                print("same time")
+                while GPIO.input(Sw_GPIO_1) == 1 and GPIO.input(Sw_GPIO_1) == 1:
+                    GPIO.output(LED_GPIO_1, GPIO.HIGH)  # LEDを点灯
+                    time.sleep(blink_interval)  # 点灯時間
+                    GPIO.output(LED_GPIO_1, GPIO.LOW)   # LEDを消灯
+                    time.sleep(blink_interval)  # 消灯時間
+                GPIO.output(LED_GPIO_1, GPIO.LOW)   # LEDを消灯
             
         elif switchStatus_1 == 1:
             # GPIO23のスイッチのみが押されている場合
@@ -54,10 +54,10 @@ while True:
             time.sleep(debounce_time)
             if GPIO.input(Sw_GPIO_2) == 1:
                 print("atui")
-                GPIO.output(LED_GPIO_2, GPIO.HIGH)  # LEDを点灯
+                GPIO.output(LED_GPIO_1, GPIO.HIGH)  # LEDを点灯
                 while GPIO.input(Sw_GPIO_2) == 1:
                     time.sleep(0.1)
-                GPIO.output(LED_GPIO_2, GPIO.LOW)  # LEDを消灯
+                GPIO.output(LED_GPIO_1, GPIO.LOW)  # LEDを消灯
         
         time.sleep(0.1)
 
