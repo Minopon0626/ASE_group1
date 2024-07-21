9451395694770acbee1acbb8d41e9c8d4b6e24c2
285112f <285112f@gs.kochi-tech.ac.jp>
2024-07-21 14:48:20 +0900

    修正

--- pushbutton_gnd.py
+++ pushbutton_gnd.py
@@ -1,20 +1,24 @@
-#タクトスイッチが押下された時の処理
-
 import time
-
-#GPIOの初期設定
 import RPi.GPIO as GPIO
+
+# GPIOの初期設定
 GPIO.setmode(GPIO.BCM)
 
-#GPIO18を入力端子設定
+# GPIO18を入力端子として設定
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-while True:
-    #スイッチ押下待ち
-    GPIO.wait_for_edge(18, GPIO.FALLING)
-
-    #画面出力
-    print('スイッチON!')
-
-    #チャタリング対策
-    time.sleep(0.3)
+try:
+    while True:
+        try:
+            # スイッチ押下待ち、デバウンス時間を設定
+            GPIO.wait_for_edge(18, GPIO.FALLING, bouncetime=200)
+            # 画面出力
+            print('スイッチON!')
+            # チャタリング対策
+            time.sleep(0.3)
+        except RuntimeError as e:
+            print(f"RuntimeError: {e}")
+except KeyboardInterrupt:
+    print("プログラムが中断されました")
+finally:
+    GPIO.cleanup()
