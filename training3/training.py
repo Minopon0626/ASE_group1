5b2668f337bccf21aedece8e38a30457e6691713
Momiyama-h <hisui.magadama@gmail.com>
2024-07-23 06:21:48 +0900

    長袖分の学習アセット準備

---
+++ training3/training.py
@@ -0,0 +1,26 @@
+# 必要なライブラリをインストール
+# pip install ultralytics
+
+# モデルのトレーニング
+from ultralytics import YOLO
+
+# モデルをロード
+model = YOLO('yolov8s.pt')  # 他のモデルサイズも使用可能
+
+# データセットのパスを設定
+data_yaml = 'data.yaml'
+
+# モデルをトレーニング
+model.train(data=data_yaml, epochs=100, imgsz=640)
+
+# モデルの検証
+model.val(data=data_yaml)
+
+# 推論の実行
+results = model('/home/hisui/academic-1/ASE_group1/training2/hansode_91.jpg')
+
+# 結果を表示
+results.show()
+
+# トレーニング済みモデルの保存
+model.save('hansode2_yolov8_model.pt')
